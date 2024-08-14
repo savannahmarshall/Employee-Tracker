23c764deb16175bb79bbb9834f972cd9cac03b1e
savannahmarshall <savvymarshall@gmail.com>
2024-08-14 13:06:56 -0700

    fixed issue with inquirer version and added prompt questions and switch cases

---
+++ index.js
@@ -0,0 +1,69 @@
+const inquirer = require('inquirer');
+
+
+// Define questions
+const questions = [
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do?',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit'
+    ],
+  }
+];
+
+// Function to start prompts
+const startPrompt = async () => {
+  const answers = await inquirer.prompt(questions);
+  
+  switch (answers.action) {
+    case 'View all departments':
+      // Call function to view all departments
+      console.log('Viewing all departments...');
+      break;
+    case 'View all roles':
+      // Call function to view all roles
+      console.log('Viewing all roles...');
+      break;
+    case 'View all employees':
+      // Call function to view all employees
+      console.log('Viewing all employees...');
+      break;
+    case 'Add a department':
+      // Call function to add a department
+      console.log('Adding a department...');
+      break;
+    case 'Add a role':
+      // Call function to add a role
+      console.log('Adding a role...');
+      break;
+    case 'Add an employee':
+      // Call function to add an employee
+      console.log('Adding an employee...');
+      break;
+    case 'Update an employee role':
+      // Call function to update an employee role
+      console.log('Updating an employee role...');
+      break;
+    case 'Exit':
+      console.log('Exiting...');
+      process.exit();
+      break;
+    default:
+      console.log('Invalid action.');
+  }
+
+  // Restart prompt after completing the action
+  startPrompt();
+};
+
+// Start prompts
+startPrompt();
