2337831c67b7ce4f5077d2dbac2a21f178ff3e7b
savannahmarshall <savvymarshall@gmail.com>
2024-08-15 23:45:45 -0700

    changed banner color

--- src/displayBanner.js
+++ src/displayBanner.js
@@ -1,8 +1,8 @@
 const displayBanner = () => {
     const reset = '\x1b[0m';
-    const pink = '\x1b[38;5;198m';
+    const darkRed = '\x1b[38;5;124m';
     console.log(`
-      ${pink}
+      ${darkRed}
   ♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥
   ♥                                                                                    ♥
   ♥                             EMPLOYEE TRACKER                                       ♥
